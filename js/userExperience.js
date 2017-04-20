c0bb11672b4d2d58ad3b9b9003a53e2a781153af
liugq <liugq5713@gmail.com>
2017-04-21 00:09:38 +0800

    按JS非侵入原则修改

--- js/userExperience.js
+++ js/userExperience.js
@@ -1,15 +1,17 @@
 //在这里我给函数参数添加了e，但是这个e是没有值的。所以直接在函数中添加event来实现取得键盘的值
-function bindEnter(e) {
-    console.log(e);
-    var btn = document.getElementById('add');
-
-    var keycode = event.keyCode;
+document.getElementById('item').addEventListener('keydown', function(e) {
+    var keycode = e.keyCode;
     if (keycode == 13) {
-        btn.click();
-        clearInput();
-        //防止用户按下回车键之后做其他的动作
-        event.returnValue = false;
+        pressEnter();
     }
+}, false);
+
+function pressEnter() {
+    var btn = document.getElementById("add");
+    btn.click();
+    clearInput();
+    //防止用户按下回车键之后做其他的动作
+    event.returnValue = false;
 }
 
 //清除输入框中的数据
