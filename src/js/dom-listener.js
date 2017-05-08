19aec4e293724a38c06e866d8cf9567202d947b1
liugq <liugq5713@gmail.com>
2017-05-08 10:33:02 +0800

    添加hasclass函数，修改了事件委托函数

--- src/js/dom-listener.js
+++ src/js/dom-listener.js
@@ -6,24 +6,40 @@ panel.addEventListener('click', function(e) {
     // console.log(e.target);
 
     //点击panel-body事件
-    if (e.target.classList[0] == 'panel-body') {
+    if (hasClass(e.target, 'panel-body')) {
         var panelBody = e.target;
         var panel = e.target.parentNode;
         panelBodyChange(id, panelBody, panel);
     }
     //点击start按钮事件
-    if (e.target.classList[1] == 'start-pause') {
+    if (hasClass(e.target, 'start-pause')) {
         var startBtn = e.target;
         startpauseBtn(id, startBtn);
     }
 
     //点击del按钮事件
-    if (e.target.classList[0] == 'panelDel') {
+    if (hasClass(e.target, 'panelDel')) {
         delBtn(id);
     }
 
     //点击edit按钮事件
-    if (e.target.classList[1] === 'set-btn') {
+    if (hasClass(e.target, 'set-btn')) {
         panelEventEdit(id);
     }
 }, false);
+
+
+//实现hasclass方法
+function hasClass(obj, cls) {
+    var obj_class = obj.className;
+    //将字符串分割成数组
+    var obj_class_lst = obj_class.split(/\s+/);
+    var x = 0;
+    //遍历数组，检测是否有该类元素
+    for (x in obj_class_lst) {
+        if (obj_class_lst[x] == cls) {
+            return true;
+        }
+    }
+    return false;
+}
