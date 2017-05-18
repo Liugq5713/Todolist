19720855cd2f335e928aeab91f246499994822d9
liugq <liugq5713@gmail.com>
2017-05-19 00:33:57 +0800

    修改了添加/删除样式函数，起名字还是很有讲究的。函数应该把动词放在前面。

--- src/js/edit-todoevent.js
+++ src/js/edit-todoevent.js
@@ -1,94 +1,94 @@
 // 检测点击edit按钮事件
 function panelEventEdit(id) {
-  formShow();
+    formShow();
     // 给表单显示一些提示信信息
-  let transaction = db.transaction(['user'], 'readwrite'),
-    storeHandler = transaction.objectStore('user');
-  storeHandler.get(id).onsuccess = function (e) {
-    const todoObj = e.target.result;
-    editEventForm.todoEventName.value = todoObj.user_event;
+    let transaction = db.transaction(['user'], 'readwrite'),
+        storeHandler = transaction.objectStore('user');
+    storeHandler.get(id).onsuccess = function(e) {
+        const todoObj = e.target.result;
+        editEventForm.todoEventName.value = todoObj.user_event;
         // 如果有标签就显示，没有就不显示
         // 关于标签，应该是放在数组里面，并且使用标签分割。
         // 添加多个标签的时候，如何使用空格将他们分开。split()
-    if (!editEventForm.eventTag.value) {
+        if (!editEventForm.eventTag.value) {
 
-    } else {
-      editEventForm.eventTag.value = todoObj.tag;
-    }
+        } else {
+            editEventForm.eventTag.value = todoObj.tag;
+        }
         // 将todo事件的id放在表单中， 方便提交， 修改数据库。
-    editEventForm.todoEventId.value = todoObj.id;
-  };
+        editEventForm.todoEventId.value = todoObj.id;
+    };
 }
 
 
 editEventForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  submitEvent();
-  showData();
+    event.preventDefault();
+    submitEvent();
+    showData();
     // 把面板关闭（隐藏）
-  formHidden();
+    formHidden();
 }, false);
 
 function submitEvent() {
-  var tags = TODO.tags;
-  console.log(tags);
+    var tags = TODO.tags;
+    console.log(tags);
     // 获取表单根元素
-  const editEventForm = document.querySelector('#editEventForm');
+    const editEventForm = document.querySelector('#editEventForm');
 
     // 获取id值
-  let id = editEventForm.todoEventId.value;
+    let id = editEventForm.todoEventId.value;
     // 注意获取到的id值是string类型的。
-  id = Number(id);
+    id = Number(id);
     // 获取修改的数据
-  const eventName = editEventForm.todoEventName.value;
-  // 获取修改后的id值
-  const tag = tags.get();
-  console.log(tags.Tags);
-  console.log(tag);
-  let transaction = db.transaction(['user'], 'readwrite'),
-    storeHandler = transaction.objectStore('user');
+    const eventName = editEventForm.todoEventName.value;
+    // 获取修改后的id值
+    const tag = tags.get();
+    console.log(tags.Tags);
+    console.log(tag);
+    let transaction = db.transaction(['user'], 'readwrite'),
+        storeHandler = transaction.objectStore('user');
 
-  const req = storeHandler.get(id);
+    const req = storeHandler.get(id);
 
-  req.onerror = function (event) {
-    console.debug(event);
-  };
-
-  req.onsuccess = function (e) {
-    const todoObj = e.target.result;
-    todoObj.user_event = eventName;
-    todoObj.tag = tag;
-    const putReq = storeHandler.put(todoObj);
-    putReq.onsuccess = function (err) {
-      console.log('edit successful');
+    req.onerror = function(event) {
+        console.debug(event);
     };
-    putReq.onerror = function () {};
-  };
+
+    req.onsuccess = function(e) {
+        const todoObj = e.target.result;
+        todoObj.user_event = eventName;
+        todoObj.tag = tag;
+        const putReq = storeHandler.put(todoObj);
+        putReq.onsuccess = function(err) {
+            console.log('edit successful');
+        };
+        putReq.onerror = function() {};
+    };
 }
 
 // 显示编辑表单
 function formShow() {
     // 获取到表单
-  const editEventForm = document.querySelector('#editEventForm');
-//获取遮罩
-  const mask=document.querySelector('.mask');
-     // 让表单显示
-  TODO.styleAdd(editEventForm,'form-show');
-  TODO.styleDel(editEventForm,'form-hidden');
+    const editEventForm = document.querySelector('#editEventForm');
+    //获取遮罩
+    const mask = document.querySelector('.mask');
+    // 让表单显示
+    TODO.addClass(editEventForm, 'form-show');
+    TODO.delClass(editEventForm, 'form-hidden');
     // 显示遮罩层
-  TODO.styleAdd(mask,'mask-show');
-  TODO.styleDel(mask,'mask-hidden');
+    TODO.addClass(mask, 'mask-show');
+    TODO.delClass(mask, 'mask-hidden');
 }
 // 隐藏编辑表单
 function formHidden() {
     // 获取到表单
-  const editEventForm = document.querySelector('#editEventForm');
+    const editEventForm = document.querySelector('#editEventForm');
     // 让表单显示
-  TODO.styleDel(editEventForm,'form-show');
-  TODO.styleAdd(editEventForm,'form-hidden');
- 
+    TODO.delClass(editEventForm, 'form-show');
+    TODO.addClass(editEventForm, 'form-hidden');
+
     // 显示遮罩层
-  const mask=document.querySelector('.mask');
-  TODO.styleAdd(mask,'mask-hidden');
-  TODO.styleDel(mask,'mask-show');
+    const mask = document.querySelector('.mask');
+    TODO.addClass(mask, 'mask-hidden');
+    TODO.delClass(mask, 'mask-show');
 }
--- src/js/node-change.js
+++ src/js/node-change.js
@@ -1,63 +1,63 @@
 // 点击panel-body事件
 function panelBodyChange(id, panelBody, panel) {
-  let transaction = db.transaction(['user'], 'readwrite'),
-    storeHander = transaction.objectStore('user');
-  storeHander.get(id).onsuccess = function (e) {
-    const todoObj = e.target.result;
-    todoObj.finished = !todoObj.finished;
-    storeHander.put(todoObj).onsuccess = function (e) {
+    let transaction = db.transaction(['user'], 'readwrite'),
+        storeHander = transaction.objectStore('user');
+    storeHander.get(id).onsuccess = function(e) {
+        const todoObj = e.target.result;
+        todoObj.finished = !todoObj.finished;
+        storeHander.put(todoObj).onsuccess = function(e) {
             // 完成的情况
-      if (todoObj.finished) {
-        TODO.styleAdd(panelBody,'eventDone');
-        TODO.styleAdd(panel,'panel-danger');
-        TODO.styleDel(panelBody,'eventTodo');
-        TODO.styleDel(panel,'panel-info');
-      }
+            if (todoObj.finished) {
+                TODO.addClass(panelBody, 'eventDone');
+                TODO.addClass(panel, 'panel-danger');
+                TODO.delClass(panelBody, 'eventTodo');
+                TODO.delClass(panel, 'panel-info');
+            }
             // 未完成的情况
-      else {
-         TODO.styleDel(panelBody,'eventDone');
-        TODO.styleDel(panel,'panel-danger');
-        TODO.styleAdd(panelBody,'eventTodo');
-        TODO.styleAdd(panel,'panel-info');
-      }
+            else {
+                TODO.delClass(panelBody, 'eventDone');
+                TODO.delClass(panel, 'panel-danger');
+                TODO.addClass(panelBody, 'eventTodo');
+                TODO.addClass(panel, 'panel-info');
+            }
+        };
     };
-  };
 }
 
 // 点击start按钮事件
 function startpauseBtn(id, startBtn) {
-  let transaction = db.transaction(['user'], 'readwrite'),
-    storeHander = transaction.objectStore('user');
-  storeHander.get(id).onsuccess = function (e) {
-    const todoObj = e.target.result;
-    todoObj.started = !todoObj.started;
-    storeHander.put(todoObj).onsuccess = function (e) {
+    let transaction = db.transaction(['user'], 'readwrite'),
+        storeHander = transaction.objectStore('user');
+    storeHander.get(id).onsuccess = function(e) {
+        const todoObj = e.target.result;
+        todoObj.started = !todoObj.started;
+        storeHander.put(todoObj).onsuccess = function(e) {
             // 完成的情况
-      const myClock = document.querySelector('#myClock');
-      if (todoObj.started) {
-        TODO.styleAdd(myClock,'show-clock');
-        TODO.styleAdd(startBtn,'pause-img');
-        TODO.styleDel(startBtn,'start-img');
-        TODO.styleDel(myClock,'no-clock');
-      }
+            const myClock = document.querySelector('#myClock');
+            if (todoObj.started) {
+                TODO.addClass(myClock, 'show-clock');
+                TODO.addClass(startBtn, 'pause-img');
+                TODO.delClass(startBtn, 'start-img');
+                TODO.delClass(myClock, 'no-clock');
+            }
             // 未完成的情况
-      else {
-        TODO.styleDel(myClock,'show-clock');
-        TODO.styleDel(startBtn,'pause-img');
-        TODO.styleAdd(startBtn,'start-img');
-        TODO.styleAdd(myClock,'no-clock');
-      }
+            else {
+                TODO.delClass(myClock, 'show-clock');
+                TODO.delClass(startBtn, 'pause-img');
+                TODO.addClass(startBtn, 'start-img');
+                TODO.addClass(myClock, 'no-clock');
+            }
+        };
     };
-  };
 }
 
 
 // 点击del按钮的事件
 function delBtn(id) {
-  let transaction = db.transaction('user', 'readwrite'),
-    storeHander = transaction.objectStore('user');
+    let transaction = db.transaction('user', 'readwrite'),
+        storeHander = transaction.objectStore('user');
     // 删除操作,需要检验一下键的类型
-  storeHander.delete(id).onsuccess = function (e) {
-    showData();
-  };
+    storeHander.delete(id).onsuccess = function(e) {
+        showData();
+    };
 }
